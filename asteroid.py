f8d301e40d48b6204acadd0eeea8bd7a3f8ae1dd
jonathanlouis318 <jonathanlouis318@gmail.com>
2025-10-27 13:14:00 -0400

    Added Player class

---
+++ asteroid.py
@@ -0,0 +1,5 @@
+from circleshap import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+        updatable.update(dt)
+        
         screen.fill("black")
+        
+        for obj in drawable:
+             obj.draw(screen)
+        
         pygame.display.flip()
+
+        dt = game_clock.tick(60) /1000
 
 
 if __name__ == "__main__":

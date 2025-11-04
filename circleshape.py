60977f627f874cb07a6424d09eb6a11cd72fcb75
jonathanlouis318 <jonathanlouis318@gmail.com>
2025-11-04 08:50:34 -0500

    added game logic

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, target):
+        radius = self.radius + target.radius
+
+        distance = pygame.math.Vector2.distance_to(self.position, target.position)
+        return radius > distance
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -13,9 +16,17 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+   
 
     while True:
         for event in pygame.event.get():
@@ -23,6 +34,15 @@ def main():
                 return
     
         updatable.update(dt)
+
+        for obj in asteroids:
+            if(obj.collision(player)):
+                print("Game Over!")
+                return
+            for bullet in shots:
+                if(obj.collision(bullet)):
+                    obj.split()
+                    bullet.kill()
         
         screen.fill("black")
         
